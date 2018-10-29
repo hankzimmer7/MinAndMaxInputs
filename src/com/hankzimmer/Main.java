e87f42c49a889a770c9d49470122ec6ba179fd97
Hank <hankzimmer7@gmail.com>
2018-10-29 11:42:41 -0400

    changes firstInput flag to a boolean rather than a counter

--- src/com/hankzimmer/Main.java
+++ src/com/hankzimmer/Main.java
@@ -8,23 +8,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int max = 0;
         int min = 0;
-        int counter = 0;
+        boolean firstInput = true;
         //Continue taking in user inputs until an invalid input, and then break out of the loop
         while (true) {
             System.out.println("Enter number");
             if (scanner.hasNextInt()) {
                 int input = scanner.nextInt();
                 //If it is the first input, set the max and min equal to the input
-                if (counter == 0) {
+                if (firstInput) {
                     min = input;
                     max = input;
-
+                    firstInput = false;
                 } else if (input < min) {
                     min = input;
                 } else if (input > max) {
                     max = input;
                 }
-                counter++;
             } else {
                 System.out.println("Invalid Input");
                 break;
